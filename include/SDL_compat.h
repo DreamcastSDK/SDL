dcaf45ad09ace37bcb792102d4ebb88b64b6573a
Sam Lantinga <slouken@libsdl.org>
2006-07-07 10:29:16 +0000

    Started on the OpenGL API revamp.

--- include/SDL_compat.h
+++ include/SDL_compat.h
@@ -171,6 +171,7 @@ extern DECLSPEC void SDLCALL SDL_UnlockYUVOverlay(SDL_Overlay * overlay);
 extern DECLSPEC int SDLCALL SDL_DisplayYUVOverlay(SDL_Overlay * overlay,
                                                   SDL_Rect * dstrect);
 extern DECLSPEC void SDLCALL SDL_FreeYUVOverlay(SDL_Overlay * overlay);
+extern DECLSPEC int SDLCALL SDL_GL_GetAttribute(SDL_GLattr attr, int *value);
 
 /* Ends C function definitions when using C++ */
 #ifdef __cplusplus
