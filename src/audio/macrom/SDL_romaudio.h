13b9afb9fd42b91dfb1523295d66ca45be40c2f1
Ryan C. Gordon <icculus@icculus.org>
2006-10-01 05:19:16 +0000

    Removed the pre-Carbon Mac OS 9 audio code, so Mac OS 8 is officially  unsupported now.  :)
    The "USE_RYANS_SOUNDCODE" portion, which is what remains, has been the
     active codepath for years now without complaint, and was used in several
     shipping OS X titles before the CoreAudio support obsoleted it, so it's a
     good fallback to keep around for testing if nothing else.
    
    This code should still work on both OS 9, and PowerPC/Intel Mac OS X.

--- src/audio/macrom/SDL_romaudio.h
+++ src/audio/macrom/SDL_romaudio.h
@@ -26,9 +26,6 @@
 
 #include "../SDL_sysaudio.h"
 
-/* This is Ryan's improved MacOS sound code, with locking support */
-#define USE_RYANS_SOUNDCODE
-
 /* Hidden "this" pointer for the video functions */
 #define _THIS	SDL_AudioDevice *this
 
@@ -36,12 +33,6 @@ struct SDL_PrivateAudioData
 {
     /* Sound manager audio channel */
     SndChannelPtr channel;
-#if defined(TARGET_API_MAC_CARBON) || defined(USE_RYANS_SOUNDCODE)
-    /* FIXME: Add Ryan's static data here */
-#else
-    /* Double buffering variables */
-    SndDoubleBufferPtr audio_buf[2];
-#endif
 };
 
 /* Old variable names */
