056c9b29a8795a8c5d4cc948977ce913263c0aa4
Holmes Futrell <hfutrell@umail.ucsb.edu>
2008-08-15 19:16:01 +0000

    Cleaned up code, added comments.

--- src/joystick/iphoneos/SDLUIAccelerationDelegate.h
+++ src/joystick/iphoneos/SDLUIAccelerationDelegate.h
@@ -1,18 +1,31 @@
-//
-//  SDLUIAccelerationDelegate.h
-//  iPodSDL
-//
-//  Created by Holmes Futrell on 6/21/08.
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
-//
+/*
+ SDL - Simple DirectMedia Layer
+ Copyright (C) 1997-2006 Sam Lantinga
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ 
+ Sam Lantinga
+ slouken@libsdl.org
+ */
 
 #import <UIKit/UIKit.h>
-#import "SDL_stdinc.h"
+#import <SDL_types.h>
 
 @interface SDLUIAccelerationDelegate: NSObject <UIAccelerometerDelegate> {
 
 	UIAccelerationValue x, y, z;
-	//NSTimeInterval timestamp;
 	BOOL isRunning;
 	BOOL hasNewData;
 	
