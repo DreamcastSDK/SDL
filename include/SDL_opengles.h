41536c832ad142cdedf889858a0119a0110ef613
Holmes Futrell <hfutrell@umail.ucsb.edu>
2008-08-15 18:31:30 +0000

    Added comments, link to Kronos site.

--- include/SDL_opengles.h
+++ include/SDL_opengles.h
@@ -1,16 +1,42 @@
-#ifndef __gl_h_
-#define __gl_h_
+/*
+ SDL - Simple DirectMedia Layer
+ Copyright (C) 1997-2006 Sam Lantinga
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ 
+ Sam Lantinga
+ slouken@libsdl.org
+ */
+
+/*
+	This is a simple file to encapsulate the OpenGL ES API headers.
+	Headers copied from The Kronos Group website.
+	http://www.khronos.org/opengles/
+ */
+
+#ifndef __gles_h_
+#define __gles_h_
 
 /* $Id: gl.h 4533 2007-11-26 11:19:35Z markc $ */
-
-//#include <GLES/glplatform.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #ifdef __IPHONEOS__
-#include <OpenGLES/ES1/gl.h>
+#include <OpenGLES/ES1/gl.h> /* Header File For The OpenGL ES Library */
 #endif
 	
 #ifndef APIENTRY
@@ -807,5 +833,5 @@ GL_API void GL_APIENTRY glPointSizePointerOES (GLenum type, GLsizei stride, cons
 }
 #endif
 
-#endif /* __gl_h_ */
+#endif /* __gles_h_ */
 
