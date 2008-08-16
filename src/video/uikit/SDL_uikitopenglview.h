1220a260172756067d752879edc1aa2c3a8097be
Holmes Futrell <hfutrell@umail.ucsb.edu>
2008-08-16 00:19:07 +0000

    whitespace

--- src/video/uikit/SDL_uikitopenglview.h
+++ src/video/uikit/SDL_uikitopenglview.h
@@ -26,9 +26,9 @@
 #import <OpenGLES/ES1/glext.h>
 #import "SDL_uikitview.h"
 /*
- This class wraps the CAEAGLLayer from CoreAnimation into a convenient UIView subclass.
- The view content is basically an EAGL surface you render your OpenGL scene into.
- Note that setting the view non-opaque will only work if the EAGL surface has an alpha channel.
+	This class wraps the CAEAGLLayer from CoreAnimation into a convenient UIView subclass.
+	The view content is basically an EAGL surface you render your OpenGL scene into.
+	Note that setting the view non-opaque will only work if the EAGL surface has an alpha channel.
  */
 @interface SDL_uikitopenglview : SDL_uikitview {
 	
