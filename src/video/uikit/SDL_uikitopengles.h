abaa9e05a7c399ea151f2f7174e12783e354a4bd
Holmes Futrell <hfutrell@umail.ucsb.edu>
2008-08-16 00:18:06 +0000

    whitespace

--- src/video/uikit/SDL_uikitopengles.h
+++ src/video/uikit/SDL_uikitopengles.h
@@ -33,4 +33,5 @@ extern SDL_GLContext UIKit_GL_CreateContext(_THIS, SDL_Window * window);
 extern void UIKit_GL_DeleteContext(_THIS, SDL_GLContext context);
 extern void *UIKit_GL_GetProcAddress(_THIS, const char *proc);
 extern int UIKit_GL_LoadLibrary(_THIS, const char *path);
+
 #endif
