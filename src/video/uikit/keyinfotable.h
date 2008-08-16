8be631b270087d68e2dad6c60d1f25a8ed7633bd
Holmes Futrell <hfutrell@umail.ucsb.edu>
2008-08-16 00:10:15 +0000

    Added comments

--- src/video/uikit/keyinfotable.h
+++ src/video/uikit/keyinfotable.h
@@ -1,16 +1,36 @@
 /*
- *  scancodes_iphoneos.h
- *  SDLiPhoneOS
- *
- *  Created by Holmes Futrell on 7/25/08.
- *  Copyright 2008 __MyCompanyName__. All rights reserved.
- *
+ SDL - Simple DirectMedia Layer
+ Copyright (C) 1997-2006 Sam Lantinga
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ 
+ Sam Lantinga
+ slouken@libsdl.org
  */
 
 #ifndef _UIKIT_KeyInfo
 #define _UIKIT_KeyInfo
 
 #include "SDL_scancode.h"
+
+/*
+	This file is used by the keyboard code in SDL_uikitview.m to convert between characters
+	passed in from the iPhone's virtual keyboard, and tuples of SDL_scancodes and SDL_keymods.
+	For example unicharToUIKeyInfoTable['a'] would give you the scan code and keymod for lower
+	case a.
+*/
 
 typedef struct {
 	SDL_scancode code;
