033b6e104b3deb159b037130aba1da0ffdfa347b
Holmes Futrell <hfutrell@umail.ucsb.edu>
2008-08-16 00:09:54 +0000

    Cleaned up, updated header include path

--- src/video/uikit/jumphack.h
+++ src/video/uikit/jumphack.h
@@ -4,7 +4,13 @@
  *
  */
 
+#ifndef _jumphack_h
+#define _jumphack_h
+
 #include "setjmp.h"
 
+/* see SDL_uikitevents.m for more info */
+
 extern jmp_buf* jump_env();
-extern int canJump;
+
+#endif
