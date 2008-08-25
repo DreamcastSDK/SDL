d0b2e777c0b812eeb64e276a6d549f1a23dc53ed
Sam Lantinga <slouken@libsdl.org>
2008-08-25 09:58:51 +0000

    Yep, 0 is the correct value here

--- src/haptic/win32/SDL_syshaptic.c
+++ src/haptic/win32/SDL_syshaptic.c
@@ -184,7 +184,7 @@ SDL_SYS_HapticInit(void)
 
    /* Look for haptic devices. */
    ret = IDirectInput_EnumDevices( dinput,
-                                   0, /* Not sure if this is legal, but gets all devices. */
+                                   0,
                                    EnumHapticsCallback,
                                    NULL, DIEDFL_FORCEFEEDBACK | DIEDFL_ATTACHEDONLY);
    if (FAILED(ret)) {
